097a63f856517e27d81a0668610f1c3c89395f1e
amitmq <amit.multiqos@gmail.com>
2024-11-19 20:45:06 +0530

    first commit2

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <h3>Hello</h3>
+        <h3>Home Page</h3>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
